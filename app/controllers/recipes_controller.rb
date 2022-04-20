ed1e027e733378aedb658a1e12379a591f00e023
Solomon Karim <93667920+Solkarim91@users.noreply.github.com>
2022-04-20 09:07:22 +0100

    fix API call

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,37 +1,32 @@
 class RecipesController < ApplicationController
   def index
+    @recipes = Recipe.all
   end
 
-  # def search
-  #   ingredients = params[:ingredients]
-  #   recipes = find_recipes
-  #   unless recipes
-  #     flash[:alert] = 'No recipes found!'
-  #     return render action: :index
-  #   end
-  #   @recipes = recipes.first(5)
-  # end
+  def search
+    ingredients = params[:ingredients]
+    recipes = find_recipes
+    unless recipes
+      flash[:alert] = 'No recipes found!'
+      return render action: :index
+    end
+    @recipes = recipes.first(5)
+  end
 
-  # private
+  private
 
-  # def request_api(url)
-  #   url = URI("https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?ingredients=apples%2C%20flour%2C%20sugar&number=5&ignorePantry=true&ranking=1")
+  def request_api(url)
+    response = URI.open(url,
+      'Content-Type' => 'application/json').read
+    p response
+    # puts response.read
 
-  #   http = Net::HTTP.new(url.host, url.port)
-  #   http.use_ssl = true
-  #   http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    # JSON.parse
+  end
 
-  #   request = Net::HTTP::Get.new(url)
-  #   request["X-RapidAPI-Host"] = 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com'
-  #   request["X-RapidAPI-Key"] = ENV['RAPIDAPI_API_KEY']
-
-  #   response = http.request(request)
-  #   puts response.read_body
-  # end
-
-  # def find_recipes
-  #   request_api(
-  #     "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?ingredients=apples%2C%20flour%2C%20sugar&number=5&ignorePantry=true&ranking=1"
-  #   )
-  # end
+  def find_recipes
+    request_api(
+      "https://api.spoonacular.com/recipes/findByIngredients?apiKey=#{ENV['SPOONACULAR_API_KEY']}&ingredients=onion"
+    )
+  end
 end
