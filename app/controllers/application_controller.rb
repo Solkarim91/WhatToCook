ef824fd56b65e8b08095ecbf6c184fecea4d5509
Solomon Karim <93667920+Solkarim91@users.noreply.github.com>
2022-06-08 14:15:48 +0100

    add controller actions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,24 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  private
+
+  def request_api(url)
+    URI.open(url,
+      'Content-Type' => 'application/json').read
+  end
+
+  def find_recipes
+    request_api(
+      "https://api.spoonacular.com/recipes/findByIngredients?apiKey=#{ENV['SPOONACULAR_API_KEY']}&ingredients=#{@ingredients.length == 1 ? @ingredients.first : @ingredients.join(",+")}"
+    )
+  end
+
+  def find_recipe
+    recipe = request_api(
+      "https://api.spoonacular.com/recipes/#{params['id']}/information?apiKey=#{ENV['SPOONACULAR_API_KEY']}"
+    )
+    @recipe = JSON.parse(recipe)
+    # @recipe_instructions = @recipe['instructions'].split('.')
+  end
 end
--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,2 +1,29 @@
 class BookmarksController < ApplicationController
+  before_action :find_recipe, only: :create
+  before_action :find_bookmark, only: :destroy
+
+  def create
+    @bookmark = Bookmark.new
+    # @recipe = Crypto.find(params[:crypto_id]) < CHANGE IF NECESSARY AS COPIED FROM CRYPTAUL
+    @bookmark.user = current_user
+    @bookmark.recipe = @recipe
+
+    if @bookmark.save!
+      redirect_back(fallback_location: root_path)
+    else
+      render 'recipes/show'
+    end
+  end
+
+  def destroy
+    @recipe = @bookmark.recipe
+    @bookmark.destroy
+    redirect_back(fallback_location: root_path)
+  end
+
+  private
+
+  def find_bookmark
+    @bookmark = Bookmark.find(params[:id])
+  end
 end
